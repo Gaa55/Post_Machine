c2071d23e960184a564029d8e373e5bcf6901047
Gaa55 <122674097+Gaa55@users.noreply.github.com>
2023-12-03 12:58:23 +0300

    commit

---
+++ app/src/main/java/com/example/study_practice/OperationCarriage.kt
@@ -0,0 +1,4 @@
+package com.example.study_practice
+
+class OperationCarriage {
+}
--- app/src/main/java/com/example/study_practice/Splash.kt
+++ app/src/main/java/com/example/study_practice/Splash.kt
@@ -3,9 +3,9 @@ package com.example.study_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class About : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        setContentView(R.layout.activity_splash)
     }
 }
